ab1a35140739dd6b88d2098eb4604fb26bbbcaa2
henryhkyim <40025988+henryhkyim@users.noreply.github.com>
2018-07-07 22:36:50 +0800

    Update StreamSample.java

--- src/com/hkyim/sample/StreamSample.java
+++ src/com/hkyim/sample/StreamSample.java
@@ -1,6 +1,8 @@
+package com.hkyim.sample;
+
 import java.util.*;
 
-public class StreamSampler {
+public class StreamSample {
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     // to find sum of even number
@@ -8,12 +10,12 @@ public class StreamSampler {
       numbers.stream()
              .filter(n -> n % 2 == 0)
              .mapToInt(n -> n)
-             .sum();
+             .sum()
     );
     System.out.println(
       numbers.stream()
              .filter(n -> n % 2 == 0)
-             .reduce(0, (total, n) -> total + n);
+             .reduce(0, (total, n) -> total + n)
     );
   }
 }   
